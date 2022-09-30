55f70b6bd74f36a6a4bc11abc416dc7047f2313c
Tassio Alves <tassioalves@jeraapps.com>
2022-09-30 15:48:54 -0400

    deploy

--- src/config.db.js
+++ src/config.db.js
@@ -1,22 +1,14 @@
 const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize({
-//   host: "localhost",
-//   database: "help_me_jera",
-//   username: "root",
-//   password: "root1",
-//   dialect: "mysql",
-// });
-
-const sequelize = new Sequelize('mysql://b55621f0b69bbc:73b2931d@us-cdbr-east-06.cleardb.net/heroku_bccfe217a74219b?reconnect=true');
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL,{dialect: "mysql",});
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Sequelize: Connection has been established successfully');
+    console.log("Sequelize: Connection has been established successfully");
   })
-  .catch(error => {
-    console.error('sequelize: Unable to connect to the database: ', error);
+  .catch((error) => {
+    console.error("sequelize: Unable to connect to the database: ", error);
   });
 
 const db = {};
