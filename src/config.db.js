229b9288a8d528f16bf7ce08f90f7d84823f7e6e
Tassio Alves <tassioalves@jeraapps.com>
2022-09-30 09:42:15 -0400

    make it better

--- src/config.db.js
+++ src/config.db.js
@@ -1,14 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize({
-  host: "localhost",
-  database: "help_me_jera",
-  username: "root",
-  password: "root1",
-  dialect: "mysql",
-});
+// const sequelize = new Sequelize({
+//   host: "localhost",
+//   database: "help_me_jera",
+//   username: "root",
+//   password: "root1",
+//   dialect: "mysql",
+// });
 
-// const sequelize = new Sequelize('mysql://b55621f0b69bbc:73b2931d@us-cdbr-east-06.cleardb.net/heroku_bccfe217a74219b?reconnect=true');
+const sequelize = new Sequelize('mysql://b55621f0b69bbc:73b2931d@us-cdbr-east-06.cleardb.net/heroku_bccfe217a74219b?reconnect=true');
 
 sequelize
   .authenticate()
